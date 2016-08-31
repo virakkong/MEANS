399bff4e97f30505f57f2e37f8e7539508f4af8a
virakkong <virakkong@virakkongs-iMac.local>
2016-08-31 14:31:19 -0700

    send text and reset network status number

--- app.js
+++ app.js
@@ -2,18 +2,36 @@
 var express = require('express');
 var app = express();
 //set port 5000
-app.set('port', 8888);
+app.set('port', 8080);
 //it run asynchronously
-//listen to port 8888
-//app.listen(8888);
+//listen to port 8080
+//app.listen(8080);
 //If want to stop listen to port:
     //1. ps -ax | grep node
     //2. kill -9 PID
 
+//set route for express
+app.get('/', function(req,res){
+   console.log("Get the hompage");
+    //send back
+   res 
+       .status(404)
+        //send text
+       .send('Express yourself');
+});
+
+app.get('/json', function(req,res){
+   console.log("Get the json");
+    //send back
+   res 
+       .status(405)
+       .json( {'jsonData': true} );
+});
+
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
     //console.log("listen to port: " + app.get('port'));
-    console.log("listen to port: " + port;
+    console.log("listen to port: " + port);
 });
 
 
