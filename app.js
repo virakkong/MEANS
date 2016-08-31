4441254ce69e631cc728b2a1c8159e2f583e32ba
virakkong <virakkong@virakkongs-iMac.local>
2016-08-31 10:04:27 -0700

    learn export module

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 require('./instantHello');
-var goodbye = require ('./talk/goodbye');
+var goodbye = require ('./talk/goodbye'); // need to include goodbye as a function name
 goodbye();
+var talk = require('./talk/');
+
+////
+talk.intro();
+
+///function with parameter
+talk.hello('virak');
--- talk/goodbye.js
+++ talk/goodbye.js
@@ -1,3 +1,6 @@
+
+//to call -->use filename without extension like goodbye(); 
+//require('./talk/goodbye'); //treat goodbye as function name
 module.exports =function() {
     console.log('Good By Virak');
 };
