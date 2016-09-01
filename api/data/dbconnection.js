6512f6db594aaa1c9d92800979e6d3fba0b9a154
virakkong <virakkong@virakkongs-iMac.local>
2016-09-01 12:50:32 -0700

    import json file into database and create skleton to connect to database at app start

---
+++ api/data/dbconnection.js
@@ -0,0 +1,16 @@
+var _connection =null;
+var open =function () {
+    
+};
+
+var get = function() {
+    
+    return _connection;
+};
+
+module.exports = {
+  open: open,
+  get: get
+    
+};
+
--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(function(req,res,next) {
     next();
 });
 
+//test 
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 //make sure the middle ware run between static and api
