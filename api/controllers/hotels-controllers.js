337e81a7ee00e78002098d991d59feb01ca05b29
virakkong <virakkong@virakkongs-iMac.local>
2016-08-31 17:45:38 -0700

    add json data and update hotell-controllers.js to call json data to display

--- api/controllers/hotels-controllers.js
+++ api/controllers/hotels-controllers.js
@@ -1,8 +1,11 @@
+//link to json data
+var hotelData = require('../data/hotel-data.json');
+
 module.exports.hotelsGetAll = function(req, res) {
    console.log("Get the hotel");
 
             //send back
             res 
                .status(200)
-               .json( {'HotelDataAll': true} );  
+               .json(hotelData);  
 };
