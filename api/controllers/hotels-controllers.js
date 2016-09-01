67ac818693c637d82bde02b4d40b98eb1b68a205
virakkong <virakkong@virakkongs-iMac.local>
2016-08-31 20:11:17 -0700

    using querystring to retrieve data, based on offset and count

--- api/controllers/hotels-controllers.js
+++ api/controllers/hotels-controllers.js
@@ -18,9 +18,26 @@ module.exports.hotelsGetOne = function(req, res) {
     //create a variable to hold individual hotel
     var thisHotel = hotelData[hotelId];
    console.log("Get the hotel Id: " + hotelId);
-
+            //set offset and count
+            var offset = 0;
+            var count = 5;
+            //check query properties are exists
+            if(req.query && req.query.offset) {
+                 //decimal based 10
+                offset = parseInt(req.query.offset,10);
+            }
+    
+             if(req.query && req.query.count) {
+                 //decimal based 10
+                count = parseInt(req.query.count,10);
+            }
+    
+            var returnData = hotelData.splice(offset, offset+count);
             //send back
             res 
                .status(200)
-               .json(thisHotel);  //http://localhost:8080/api/hotels/thisHotel
+//             .json(thisHotel);  //http://localhost:8080/api/hotels/thisHotel
+            
+               .jons(returnData); 
+                //http://localhost:8080/api/hotels?offset=2&count=3
 };
