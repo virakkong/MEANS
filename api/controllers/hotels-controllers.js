048df5aa0814133dfc7b399b61aeab527ef75e2d
virakkong <virakkong@virakkongs-iMac.local>
2016-08-31 18:09:58 -0700

    get hotelId based on index of array in json data

--- api/controllers/hotels-controllers.js
+++ api/controllers/hotels-controllers.js
@@ -7,5 +7,20 @@ module.exports.hotelsGetAll = function(req, res) {
             //send back
             res 
                .status(200)
-               .json(hotelData);  
+               .json(hotelData);  //http://localhost:8080/api/hotels
+};
+
+//create controller for hotelId
+module.exports.hotelsGetOne = function(req, res) {
+    //we want localhost/data/hotels/hotelId
+    var hotelId =req.params.hotelId;
+    
+    //create a variable to hold individual hotel
+    var thisHotel = hotelData[hotelId];
+   console.log("Get the hotel Id: " + hotelId);
+
+            //send back
+            res 
+               .status(200)
+               .json(thisHotel);  //http://localhost:8080/api/hotels/thisHotel
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -7,10 +7,18 @@ var router = express.Router();  //localhost:8080/api/
 
 //set controllers
 var CtrlHotels = require('../controllers/hotels-controllers.js');
+
 router 
     .route('/hotels')   //localhost:8080/api/hotels
 
     //call controller to use
     .get(CtrlHotels.hotelsGetAll);  //hotelsGetAll is a public method exported by module
-     
+
+router
+    //define parameter
+    .route('/hotels/:hotelId')   //localhost:8080/api/hotels/<hotelId>
+
+    //call controller to use
+    .get(CtrlHotels.hotelsGetOne);  //add new controller
+
 module.exports = router;
